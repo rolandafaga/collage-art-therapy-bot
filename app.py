ea0d0777fb4fba5c4b8bb6ba71de267baf361e36
Roland Afaga <rolandafaga@gmail.com>
2022-04-28 12:00:08 -0700

    added chatbot to the sidebar

--- app.py
+++ app.py
@@ -2,15 +2,24 @@
 from flask import Flask, render_template, request
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
-
 import spacy.cli 
 
-app = Flask(__name__, template_folder='templates')
+import json 
+import numpy as np 
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Embedding, GlobalAveragePooling1D
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from sklearn.preprocessing import LabelEncoder
+import pickle
+from imageai.Classification.Custom import ClassificationModelTrainer
 
-#create chatbot
-englishBot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
-trainer = ChatterBotCorpusTrainer(englishBot)
-trainer.train("chatterbot.corpus.english") #train the chatter bot for english
+import train
+from train import *
+
+app = Flask(__name__, template_folder='templates')
 
 #define app routes
 @app.route("/")
@@ -21,7 +30,19 @@ def index():
 #function for the bot response
 def get_bot_response():
     userText = request.args.get('msg')
-    return str(englishBot.get_response(userText))
+    # return str(englishBot.get_response(userText))
+    result = model.predict(pad_sequences(tokenizer.texts_to_sequences([userText]), truncating='post', maxlen=max_len))
+    category = lbl_encoder.inverse_transform([np.argmax(result)]) # labels[np.argmax(result)]
+    for i in data['intents']:
+        if i['tag']==category:
+            return(np.random.choice(i['responses']))
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="localhost", port=8000, debug=True)
+    
+#PythonApplicattion17
+# def look():
+#     print("Looking at your wonderful piece! Give me a second...")
+# 
+# look()
